4b07c4447026df266b4a77d7cc73d8ad6c8e40c1
SamMahani <sam.amirmahani@gmail.com>
2018-06-01 10:45:52 -0700

    got create, update to work

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
- $('.submitForm').on('click', function(){
- 	let $textField = $('.textField').val();
-    $('.debug').text(textFieldValue)
- })
+
+	 $('.setData').on('click', function(){
+	 	let textFieldValue = $('.textField').val();
+	    //$('.debug').text(textFieldValue)
+
+	    localStorage.setItem('myFormTextData', textFieldValue);
+	    $('.textField').val('');
+	 });
+
+	 $('.getData').on('click', function() {
+	 	let retrievedData = localStorage.getItem('myFormTextData');
+	 	$('.debug').text(retrievedData);
+	 });
+
+
+	//debut
+	// $('.textField').on('keyup', function() {
+	// 	let textFieldValue = $('.textField').val();
+	//     $('.debug').text(textFieldValue)
+	// });
+
+
+
+
 });

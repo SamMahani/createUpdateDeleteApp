bd454ba64220b69537fd797539d41ebcc3046314
SamMahani <sam.amirmahani@gmail.com>
2018-06-07 03:22:29 -0700

    project complete

--- README.md
+++ README.md
@@ -6,13 +6,8 @@ Goals:
 - [x] click handler to capture data in text form field
 - [x] helper utilities to read and write to local storage
 
-- [ ] Make it handle code! (use a textarea insted of a text)
-- [ ] Have two fields, one is a short name the other is the snippet
-- [ ] going to store the name and code inside the localStorage
-- [ ] display all snippets
-- [ ] search for a snipped using the name
-- [ ] 
-- [ ] 
-- [ ] 
-- [ ] 
-- [ ] 
+- [x] Make it handle code! (use a textarea insted of a text)
+- [x] Have two fields, one is a short name the other is the snippet
+- [x] going to store the name and code inside the localStorage
+- [x] display all snippets
+- [x] search for a snipped using the name
--- app.js
+++ app.js
@@ -2,24 +2,68 @@ $(document).ready(function(){
 
 
 	 $('.getData').on('click', function() {
-	 	let searchFieldValue = $('.searchField').val(); //saved user input
-	 	let retrievedData = localStorage.getItem(`${searchFieldValue}`);
+	 	$('.outputContainer').html('');
+	 	let $outputContainer = $('.outputContainer');
+	 	let searchVal = $('.searchField').val(); //saved user input
+	 	let getVal = JSON.parse(localStorage.getItem(`${searchVal}`));
+
+	 	if(getVal === null) {
+	 		alert("Incorrect use input, please try again");
+	 	}
+
+	 	else {
+	 		//for each instance of code, insert into site
+		 	for(let key in getVal) {
+		 		console.log(key, typeof key);
+		 		if(key !== 'count') {
+		 			let $insertThis = $(`<textarea class="codeSnippets">${getVal[key]}</textarea>`);
+		 			$insertThis.prependTo($outputContainer);
+		 		}
+	 		}//for
+	 	}//else
 
 
-	 	console.log(retrievedData)
-	 	if(retrievedData === null) {
-	 		alert("Incorrect use input, try again");
-	 	};
-
-	 	$('.insertCode').text(retrievedData);
+	 	//clear search field
 	 	$('.searchField').val('');
 
 	 });
 
+
+
+
 	 $('.setData').on('click', function(){
-	 	let nameFieldValue = $('.nameField').val();
-	 	let codeFieldValue = $('.codeField').val();
-	    localStorage.setItem(`${nameFieldValue}`, codeFieldValue);
+	 	//define nameVal and codeVal to user's input
+	 	let nameVal = $('.nameField').val();
+	 	let codeVal = $('.codeField').val();
+	 	let setVal = '{}';
+
+	 	//retreive parsed data at key nameVal  
+		let getVal = JSON.parse(localStorage.getItem(`${nameVal}`));
+
+	    //if name does not exist as key (is null)
+	 	if(getVal === null) {
+	        //create new object
+	 		let newObj = {
+	 			count: 1,
+	 		    1: codeVal,
+	 		}
+	        //set setVal to stringified new object
+	 		setVal = JSON.stringify(newObj);
+	 	}
+
+	    //if name exists as key (not null)
+	    else if(getVal !== null) {
+	    	let newCount = ++getVal.count;
+	    	//add new key as obj[++count] = new code value
+	    	getVal[newCount] = codeVal;
+	    	//set setVal to stringified updated object
+	    	setVal = JSON.stringify(getVal);
+	    }
+
+	    //set local storage at key to setVal
+	    localStorage.setItem(`${nameVal}`, setVal);
+
+	    //clear name and code fields
 	    $('.nameField').val('');
 	    $('.codeField').val('');
 
